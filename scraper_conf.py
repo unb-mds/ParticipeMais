0ab4076acb54f69d6157b5c6adb6fe2caef95292
Giovanni Ferreira <giovannidornelasf@gmail.com>
2025-04-30 19:42:44 -0300

    Update scraper_conf.py

--- scraper_conf.py
+++ scraper_conf.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
