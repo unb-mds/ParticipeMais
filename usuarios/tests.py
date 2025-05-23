cf1fa838d36c0e82193b038bc9e70f09684d6417
Szervinsk <mathszer1103@gmail.com>
2025-05-22 23:51:24 -0300

    Introdução de Testes e Documentação

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from usuarios.models import Usuario
 
-# Create your tests here.
+class UsuarioTests(APITestCase):
+
+    def setUp(self):
+        self.url = reverse('listar_usuarios')  # usa o nome da URL
+
+    def test_listar_usuarios(self):
+        # Cria um usuário no banco de teste
+        Usuario.objects.create_user(
+            nome='João', email='joao@email.com', password='senha123', data_nascimento='2000-01-01'
+        )
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        print("Resposta JSON:", response.json())  # imprime no terminal
