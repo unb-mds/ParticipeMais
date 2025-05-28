17b68f6d726b86375b84156f36e86dacab3f32f9
Pedro <pedrohg2609@gmail.com>
2025-05-28 20:05:21 -0300

    feat: Foi criada uma view para visualizar os pontos dos usuários. Implementada também uma função que atualiza a pontuação toda vez que um comentário é criado. Ainda falta implementar a lógica para atualização de pontos nas curtidas, mas a base de atualização de pontuação já está estruturada.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('logout/', Logout.as_view(), name="logout"),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('notifications/', NotificationsView.as_view(), name='notifications'),
     path('forgotpassword/', RequestEmail.as_view(), name="request_email"),
     path('forgotpassword/confirmtoken/<uidb64>/<token>', CheckToken.as_view(), name="check_token"),
     path('forgotpassword/setnewpassword/<uidb64>/<token>', SetNewPassword.as_view(), name='set_new_password'),
