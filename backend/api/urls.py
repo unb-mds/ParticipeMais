b61b203992c1b4f91e06c24ce68c3c67a37f00a2
Szervinsk <mathszer1103@gmail.com>
2025-05-28 10:51:09 -0300

    Notificação de boas vindas ao fazer login

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,8 +8,8 @@ router.register(r'pesquisar', PesquisarView, basename='pesquisar')
 
 urlpatterns = [
     
-    path('' , Home.as_view(), name='home',),
-    path('home', DescubraView.as_view(), name='descubra_page'),
+    path('' , Home.as_view(), name='home'),
+    path('descubra/', DescubraView.as_view(), name='descubra_page'),
     path('cadastro/', CadastroView.as_view()),
     # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/', Login.as_view(), name='login'),
@@ -19,8 +19,6 @@ urlpatterns = [
     path('forgotpassword/', RequestEmail.as_view(), name="request_email"),
     path('forgotpassword/confirmtoken/<uidb64>/<token>', CheckToken.as_view(), name="check_token"),
     path('forgotpassword/setnewpassword/<uidb64>/<token>', SetNewPassword.as_view(), name='set_new_password'),
-    
-    
     
 ]
 
