2036587cd343726323a4b2f34308d8edf030ad0a
Szervinsk <mathszer1103@gmail.com>
2025-06-12 16:13:03 -0300

    Add ml

--- backend/consultas/urls.py
+++ backend/consultas/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 from api.views import ConsultasViewSet
-from .views import ListaConsultas, AcessaConsulta, AcessaPropostasConsultas, PropostaDiretaC
+from .views import *
 
 router = DefaultRouter()
 router.register(r'', ConsultasViewSet, basename='consultas')
@@ -9,6 +9,6 @@ router.register(r'', ConsultasViewSet, basename='consultas')
 urlpatterns = [
     path('', ListaConsultas.as_view(), name='Lista_consulta'),
     path('<int:pk>/', AcessaConsulta.as_view(), name='Acessa_consulta'),
-    path('<int:pk>/propostas/', AcessaPropostasConsultas.as_view(), name='Acessa_propostas_consulta') ,   
+    path('<int:pk>/propostas/', AcessaPropostasConsultas.as_view(), name='Acessa_propostas_consulta'),   
     path('<int:pk>/propostas/<int:jk>', PropostaDiretaC.as_view(), name='Proposta_Direta_C')    
 ]
