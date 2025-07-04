f48bd9fef4beee7b875c9f32ff33f287fc05fe67
Pedro <pedrohg2609@gmail.com>
2025-07-04 02:12:04 -0300

    mudancas smtp

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -111,10 +111,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'participemais.mds@gmail.com'
-EMAIL_HOST_PASSWORD = 'izva gvsv qyhb leek '
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 #=================================================================
 
